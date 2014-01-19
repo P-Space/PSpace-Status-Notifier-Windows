4cbb8e23abf04ee6e5d5724d1d594b2d45fc6f61
Epameinondas Rontogiannis <eparon@live.com>
2014-01-19 03:39:23 +0200

    adding the ability to autostart the app with windows startup. coming soon: settings menu.

--- PSpaceStatusChanger/Program.cs
+++ PSpaceStatusChanger/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace PSpaceStatusChanger
 {
@@ -12,6 +13,8 @@ namespace PSpaceStatusChanger
         static int oldstatus = -5;
         static void Main()
         {
+            EnableAutoStartup();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -44,6 +47,39 @@ namespace PSpaceStatusChanger
                 }
                 oldstatus = newstatus;
             }
+        }
+
+        static void EnableAutoStartup()
+        {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (!IsStartupItem())
+                // Add the value in the registry so that the application runs at startup
+                rkApp.SetValue("PSpaceNotifier", Application.ExecutablePath.ToString());
+        }
+
+        static void DisableAutoStartup()
+        {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (IsStartupItem())
+                // Remove the value from the registry so that the application doesn't start
+                rkApp.DeleteValue("PSpaceNotifier", false);
+        }
+
+        private static bool IsStartupItem()
+        {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (rkApp.GetValue("PSpaceNotifier") == null)
+                // The value doesn't exist, the application is not set to run at startup
+                return false;
+            else
+                // The value exists, the application is set to run at startup
+                return true;
         }
     }
 }
